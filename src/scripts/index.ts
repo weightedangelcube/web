2d6814cf79bf6f509e527901179f09b9fb23efaf
weightedangelcube <angel@angelcube.dev>
2025-03-02 16:32:17 -0700

    fix: proxy cors url query

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,18 +1,16 @@
-
-
 async function fetchCarbon() : Promise<number> {
-    const url : string = "https://corsproxy.io/?https://api.websitecarbon.com/site?url=" + window.location.hostname;
-    const response : Response = await fetch(url, {
-        method: "GET"
-    });
+    const url : string = "https://corsproxy.io/?url=https://api.websitecarbon.com/site?url=" + window.location.hostname;
+
+    const response : Response = await fetch(url);
+    console.log(response);
     return await response.json().then(data => data.statistics.co2.grid.litres);
 }
 
 window.onload = async function() {
     try {
         document.getElementById('carbon').innerHTML = "your visit to this website produced " + Math.round(await fetchCarbon() * 1000)  + " mL of carbon dioxide!";
-    } catch {
+    } catch (error) {
         document.getElementById('carbon').innerHTML = "unable to load carbon usage!";
+        console.log(error);
     }
-
 };
