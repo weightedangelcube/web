d8177b57160511a0783f342f82cd58707aeb0265
weightedangelcube <angel@angelcube.dev>
2025-03-22 20:19:28 -0600

    chore: add redirects for old URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://angelcube.dev"
+  site: "https://angelcube.dev",
+
+  redirects: {
+      '/projects.html': '/projects',
+      '/contact.html': '/contact'
+	},
 });
