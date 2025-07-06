24c2e3c5d3a55ed758749b01d97d18e2a9ceae26
weightedangelcube <angel@angelcube.dev>
2025-07-05 20:12:57 -0600

    fix: last played song from backend

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,16 +1,26 @@
-async function fetchCarbon() : Promise<number> {
-    const url : string = "https://corsproxy.io/?url=https://api.websitecarbon.com/site?url=" + window.location.hostname;
+window.onload = async function() {
+    let tunesList = document.getElementById('tunes-list');
+    let innerHTML = '';
+    let musicResponse = (await fetch("https://corsproxy.io/?url=https://backend.angelcube.dev/music"))
+        .json()
+        .then(data => data.recenttracks.track.forEach(track => {
+            innerHTML +=
+                `<li class="tunes-item">
+                    <img src="${track.image[0]["#text"]}"></img>
+                    <div class="tunes-item-meta">
+                        <h4>${track.name}</h4>
+                        <p>${track.artist["#text"]}</p>
+                    </div>
+                </li>`;
+        })).then(function () {
+            tunesList.innerHTML = innerHTML;
+        });
+    
 
-    const response : Response = await fetch(url);
-    console.log(response);
-    return await response.json().then(data => data.statistics.co2.grid.litres);
-}
-
-/* window.onload = async function() {
-    try {
-        document.getElementById('carbon').innerHTML = "your visit to this website produced only " + Math.round(await fetchCarbon() * 100000) / 100  + " mL of carbon dioxide!";
-    } catch (error) {
-        document.getElementById('carbon').innerHTML = "unable to load carbon usage!";
-        console.log(error);
-    }
-}; */
+    // try {
+    //     document.getElementById('carbon').innerHTML = "your visit to this website produced only " + Math.round(await fetchCarbon() * 100000) / 100  + " mL of carbon dioxide!";
+    // } catch (error) {
+    //     document.getElementById('carbon').innerHTML = "unable to load carbon usage!";
+    //     console.log(error);
+    // }
+}; 
