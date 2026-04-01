6fa88f6c7a3af31ffc3589bf5af6541c88b4d4ad
Angel Wang <angel@angelcube.dev>
2026-04-01 11:18:24 -0600

    feat(treewide): tracking, superscript, and other cool styles

--- src/scripts/LocalTime.tsx
+++ src/scripts/LocalTime.tsx
@@ -3,15 +3,23 @@ import { Temporal } from "temporal-polyfill"
 
 export default function LocalTime() {
     const [time, setTime] = useState("")
+    const [timezone, setTimezone] = useState("")
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         setInterval(() => {
-            setTime(Temporal.Now.zonedDateTimeISO("America/Edmonton").toLocaleString())
+            setTime(Temporal.Now.zonedDateTimeISO("America/Edmonton").toPlainDateTime().toLocaleString("en-CA", { hourCycle: "h24" }))
+            setLoading(false);
+
+            setTimezone("UTC" + Temporal.Now.zonedDateTimeISO("America/Edmonton").offset)
+
         }, 1000)
     })
 
+    if (loading) return;
+
     return (
-        <p className="local-time">{time}</p>
+        <p className="local-time">{time} <sup>{timezone}</sup></p>
     )
     
 }
